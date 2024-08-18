1841efc8dddf7498d9c47a297e2c0284ecd729bd
danielgaim-visma <daniel.gaim@visma.com>
2024-08-18 23:15:06 +0200

    Updated procfile

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from logging.handlers import RotatingFileHandler
+from flask import send_from_directory
 from app import create_app
 from app.config import Config
 
@@ -29,5 +30,14 @@ if not app.debug:
     app.logger.setLevel(logging.INFO)
     app.logger.info('WebDoc Parser startup')
 
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=Config.DEBUG)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)), debug=Config.DEBUG)
